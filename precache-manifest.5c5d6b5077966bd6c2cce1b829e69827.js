b4adcc05850474977629dcf2ff927298c54baefd
Marek Marušin <marusin.marek@gmail.com>
2019-10-29 11:05:32 +0100

    Updates

---
+++ precache-manifest.5c5d6b5077966bd6c2cce1b829e69827.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7612ff750e405b1b213",
+    "url": "/strom/static/css/main.3ccd3221.chunk.css"
+  },
+  {
+    "revision": "b7612ff750e405b1b213",
+    "url": "/strom/static/js/main.b7612ff7.chunk.js"
+  },
+  {
+    "revision": "19dde04c23cfab38d715",
+    "url": "/strom/static/js/runtime~main.19dde04c.js"
+  },
+  {
+    "revision": "5e75981bcab6d436ad42",
+    "url": "/strom/static/js/2.5e75981b.chunk.js"
+  },
+  {
+    "revision": "4afd23ed070bd73a60a3af73d04b2231",
+    "url": "/strom/static/media/body-bg-blur2.4afd23ed.jpg"
+  },
+  {
+    "revision": "dd1a933ba0287e824ec3f84eeb53a8f6",
+    "url": "/strom/static/media/body-bg-blur1.dd1a933b.jpg"
+  },
+  {
+    "revision": "b203eabfb0a5c8c9c5fc0ef277b8511e",
+    "url": "/strom/static/media/body-bg-blur.b203eabf.jpg"
+  },
+  {
+    "revision": "c3bcd8e58358b02acb1803791408e63f",
+    "url": "/strom/static/media/jasna_ndvi.c3bcd8e5.png"
+  },
+  {
+    "revision": "d20b71d322cfd22a994f8dd854330665",
+    "url": "/strom/static/media/jasna_sat_2015.d20b71d3.png"
+  },
+  {
+    "revision": "55128dfd67cbb72f07d84c8991b80bde",
+    "url": "/strom/static/media/jasna_sat_2019.55128dfd.png"
+  },
+  {
+    "revision": "d402a437ff5209062abaf6ca3f00bb71",
+    "url": "/strom/static/media/sumava_ndvi.d402a437.png"
+  },
+  {
+    "revision": "2aff946b78b31d9c6641a5a0e6acf9b1",
+    "url": "/strom/static/media/satelite_2018.2aff946b.png"
+  },
+  {
+    "revision": "892781d9afdbe629e2b849a536b31637",
+    "url": "/strom/static/media/decin_sentinel1.892781d9.png"
+  },
+  {
+    "revision": "f62e89c046fe408585d11bfe0da7974a",
+    "url": "/strom/static/media/decin_sat.f62e89c0.png"
+  },
+  {
+    "revision": "95dd12f98865acca79e45917f37bb2ae",
+    "url": "/strom/static/media/ice_cream.95dd12f9.svg"
+  },
+  {
+    "revision": "ba823f7a8cedbaeb7a4b3006d62521e6",
+    "url": "/strom/static/media/liptov_termo.ba823f7a.png"
+  },
+  {
+    "revision": "e56d10801bcf55e9dd6b4eaffadd2ee6",
+    "url": "/strom/static/media/logo-z-transparent.e56d1080.png"
+  },
+  {
+    "revision": "7b535aa82a9fadd20ead6f54530e1f8e",
+    "url": "/strom/static/media/head-bg3.7b535aa8.jpg"
+  },
+  {
+    "revision": "bfa7d2a2eeaa09ff801c6003f4133d28",
+    "url": "/strom/index.html"
+  }
+];
